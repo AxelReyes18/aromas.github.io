529eaec5db0d9a0b8e0776ef9ee14e625310792c
AxelReyes18 <138253985+AxelReyes18@users.noreply.github.com>
2023-11-10 13:24:28 -0300

    Add files via upload

---
+++ formreg.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Aromatica</title>
+        <link rel="stylesheet" href="formreg.css">
+        <link rel="stylesheet" href="fontello.css">
+    </head>
+    <body>
+        <section class="formulario-registro">
+            <h4>Formulario de Registro</h4>
+            <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="ingrese su apellido">
+            <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="ingrese su nombre">
+            <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="ingrese su email">
+            <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="ingrese su contraseña">
+            <p>Estoy de acuerdo <a href="#"> Terminos y Condiciones</a></p>
+            <input class="botons" type="submit" value="Registrar">
+            <p><a href="#">Ya tengo cuenta</a></p>
+        </section>
+    </body>
